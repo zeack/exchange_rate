66f81cb32abcc945e0763aca4bd64049e9a365a4
zeack <zeack.dev@gmail.com>
2022-01-30 15:04:17 -0600

    ADD static setting for heroku

--- exchange_rate/settings.py
+++ exchange_rate/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = f'{BASE_DIR}/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/users/'
